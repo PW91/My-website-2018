1e5997d9b4439a62da8badd00ef38fe527b426f2
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2018-12-10 12:01:00 +0100

    initial

---
+++ src/_scripts/main.js
@@ -0,0 +1,106 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+"use strict";
+
+import $ from "jquery";
+import Link from "../_modules/link/link";
+
+$(() => {
+  new Link(); // Activate Link modules logic
+
+  const helloSectionEl = document.getElementsByClassName("section-hello")[0];
+  const aboutSectionEl = document.getElementsByClassName("section-about")[0];
+  const expSectionEl = document.getElementsByClassName("section-exp")[0];
+  const skillsSectionEl = document.getElementsByClassName("section-skills")[0];
+  const contactSectionEl = document.getElementsByClassName(
+    "section-contact"
+  )[0];
+
+  const forHelloEl = document.getElementsByClassName("for-hello")[0];
+  const forAboutEl = document.getElementsByClassName("for-about")[0];
+  const forExpEl = document.getElementsByClassName("for-exp")[0];
+  const forSkillsEl = document.getElementsByClassName("for-skills")[0];
+  const forContactEl = document.getElementsByClassName("for-contact")[0];
+
+  const helloNavEl = document.getElementsByClassName("nav-item-hello")[0];
+  const aboutNavEl = document.getElementsByClassName("nav-item-about")[0];
+  const expNavEl = document.getElementsByClassName("nav-item-exp")[0];
+  const skillsNavEl = document.getElementsByClassName("nav-item-skills")[0];
+  const contactNavEl = document.getElementsByClassName("nav-item-contact")[0];
+
+  const navDotEl = document.getElementsByClassName("nav-dot")[0];
+
+  let scrollingFlag = "";
+
+  document.addEventListener("scroll", () => {
+    const scroll = window.scrollY - window.innerHeight / 2;
+    if (scroll > skillsSectionEl.offsetTop) {
+      if (scrollingFlag !== "contact") {
+        setFlag("contact");
+        removeClasses();
+        setClassList(forContactEl);
+        setClassList(contactNavEl);
+        moveDot("250px");
+      }
+    } else if (scroll > expSectionEl.offsetTop) {
+      if (scrollingFlag !== "exp") {
+        setFlag("exp");
+        removeClasses();
+        setClassList(forSkillsEl);
+        setClassList(skillsNavEl);
+        moveDot("197px");
+      }
+    } else if (scroll > aboutSectionEl.offsetTop) {
+      if (scrollingFlag !== "skills") {
+        setFlag("skills");
+        removeClasses();
+        setClassList(forExpEl);
+        setClassList(expNavEl);
+        moveDot("142px");
+      }
+    } else if (scroll > helloSectionEl.offsetTop) {
+      if (scrollingFlag !== "about") {
+        setFlag("about");
+        removeClasses();
+        setClassList(forAboutEl);
+        setClassList(aboutNavEl);
+        moveDot("88px");
+      }
+    } else {
+      if (scrollingFlag !== "hello") {
+        setFlag("hello");
+        removeClasses();
+        setClassList(forHelloEl);
+        setClassList(helloNavEl);
+        moveDot("32px");
+      }
+    }
+  });
+
+  function moveDot(where) {
+    navDotEl.style.top = where;
+  }
+
+  function setClassList(el) {
+    el.classList.add("active");
+  }
+
+  function setFlag(flag) {
+    scrollingFlag = flag;
+  }
+
+  function removeClasses() {
+    forHelloEl.classList.remove("active");
+    forAboutEl.classList.remove("active");
+    forExpEl.classList.remove("active");
+    forSkillsEl.classList.remove("active");
+    forContactEl.classList.remove("active");
+
+    helloNavEl.classList.remove("active");
+    aboutNavEl.classList.remove("active");
+    expNavEl.classList.remove("active");
+    skillsNavEl.classList.remove("active");
+    contactNavEl.classList.remove("active");
+  }
+});
